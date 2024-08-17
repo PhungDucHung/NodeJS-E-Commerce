3f7c698ffb831b54ceea06263e7bbed7c0234876
Phung Duc Hung <phungduchung9808@gmail.com>
2024-08-17 23:13:23 +0700

    Write api Update User by User

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -156,6 +156,17 @@ const deleteUser = asyncHandler(async (req, res) => {
     })
 })
 
+const updateUser = asyncHandler(async (req, res) => {
+    // 
+    const { _id } = req.user
+    if (!_id || Object.keys(req.body).length === 0) throw new Error('Missing inputs')
+    const response = await User.findByIdAndUpdate(_id, req.body, { new: true }).select('-password -role -refreshToken')
+    return res.status(200).json({
+        success: response ? true : false,
+        updatedUser: response ? response : 'Some thing went wrong'
+    })
+})
+
 module.exports = {
-    register, login ,getCurrent ,refreshAccessToken , logout ,forgotPassword ,resetPassword ,getUsers ,deleteUser
+    register, login ,getCurrent ,refreshAccessToken , logout ,forgotPassword ,resetPassword ,getUsers ,deleteUser ,updateUser
 };
