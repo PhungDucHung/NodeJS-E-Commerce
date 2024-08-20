37d2e05ae7bdce89a9970fb413c70c43d7a8f75f
Phung Duc Hung <phungduchung9808@gmail.com>
2024-08-20 15:27:18 +0700

    Update Likes & Write Dislikes

--- server/models/blog.js
+++ server/models/blog.js
@@ -24,6 +24,12 @@
             ref: 'User'
         }
      ],
+     dislikes: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'User'
+        }
+     ],
      image: {
         type: String,
         default: 'https://t4.ftcdn.net/jpg/03/61/81/57/360_F_361815765_y0iTbtBthz6UOcwILdCflYvjagqLWWRK.jpg'
