d3420991ecbf4be4b1198e9ee9e121fbaac26e6c
Phung Duc Hung <phungduchung9808@gmail.com>
2024-08-21 18:31:14 +0700

    Upload image Blog

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router(); // Thay đổi ở đây
 const {verifyAccessToken , isAdmin} = require('../middlewares/verifyToken');
 const ctrls = require('../controllers/blog')
+const uploader = require('../config/cloudinary.config')
 
     router.get('/', ctrls.getBlogs);
     router.post('/', [verifyAccessToken , isAdmin], ctrls.createNewBlog);
     router.get('/one/:bid', ctrls.getBlog);
     router.put('/like/:bid', [verifyAccessToken ], ctrls.likeBlog);
+    router.put('/image/:bid', [verifyAccessToken , isAdmin ], uploader.single('image'), ctrls.uploadImagesBlog);
     router.put('/dislike/:bid', [verifyAccessToken ], ctrls.dislikeBlog);
     router.put('/:bid', [verifyAccessToken , isAdmin], ctrls.updateBlog);
     router.delete('/:bid', [verifyAccessToken , isAdmin], ctrls.deleteBlog);
