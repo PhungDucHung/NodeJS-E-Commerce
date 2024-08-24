de90ca3f7af3e6cccca9359237fbd80a466a227e
Phung Duc Hung <phungduchung9808@gmail.com>
2024-08-24 10:39:59 +0700

    Write api get User order & get oders

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -12,7 +12,7 @@ const createOrder = asyncHandler(async(req,res)=>{
         count: el.quantity,
         color: el.color
     }))
-    const total = userCart?.cart?.reduce((sum, el) => el.product.price*el.quantity + sum , 0)
+    let total = userCart?.cart?.reduce((sum, el) => el.product.price*el.quantity + sum , 0)
     if(coupon) total = Math.round(total * (1 - coupon/100)/1000)*1000
     const rs = await Order.create({ products, total, orderBy: _id})
     return res.json({
@@ -22,6 +22,34 @@ const createOrder = asyncHandler(async(req,res)=>{
     })
 })
 
+const updateStatus = asyncHandler(async(req,res)=>{
+    const {oid} = req.params
+    const {status} = req.body
+    if(!status) throw new Error('Missing status')
+    const response = await Order.findByIdAndUpdate(oid, {status} , {new: true})
+    return res.json({
+        success: response ? true : false,
+        response : response ? response : 'Something went wrong',
+    })
+})
+
+const getUserOrder = asyncHandler(async(req,res)=>{
+    const {_id} = req.user
+    const response = await Order.find({orderBy: _id})
+    return res.json({
+        success: response ? true : false,
+        response : response ? response : 'Something went wrong',
+    })
+})
+
+const getOrders = asyncHandler(async(req,res)=>{
+    const response = await Order.find()
+    return res.json({
+        success: response ? true : false,
+        response : response ? response : 'Something went wrong',
+    })
+})
+
 module.exports = {
-    createOrder
+    createOrder, updateStatus ,getUserOrder ,getOrders
 }
